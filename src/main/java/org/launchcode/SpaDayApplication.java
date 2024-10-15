fd12bf6326e315ffdfc114669e5104a71e3baa2c
Stephen Howard <Showard1793@gmail.com>
2024-10-14 19:11:27 -0500

    Updated

--- src/main/java/org/launchcode/SpaDayApplication.java
+++ src/main/java/org/launchcode/SpaDayApplication.java
@@ -3,6 +3,7 @@ package org.launchcode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Test
 @SpringBootApplication
 public class SpaDayApplication {
 
